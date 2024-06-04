c46f814d5800a75e155099ca73e7763725ea4fe4
Sebbybobbler <seb_davies@hotmail.co.uk>
2024-06-04 10:06:37 +0100

    get minion route

--- server/api.js
+++ server/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const apiRouter = express.Router();
+const {getAllFromDatabase} = require("./db.js")
 
+// Minions
+apiRouter.get("/minions", (req,res,next)=>{
+    res.status(200).send(getAllFromDatabase("minions"))
+})
 
 
 module.exports = apiRouter;
